bd89ed352fabab1f8ec5dd101157619b474f42cb
suwolff <susann.wolff@gmx.net>
2025-05-29 12:32:16 +0200

    Updated Arrays with info from Microsoft Learn

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -15,15 +15,19 @@ namespace Arrays
         {
             //1D Arrays
 
-            string[] names = new string[] { "Felinebiene", "Mamakatze", "Papahase" };
+            string[] names = new string[] { "Felinebiene", "Mamakatze", "Papahase" };   // INITIALIZE (alte Version)
             int[] ages = new int[] { 9, 49, 44 };
 
-            int[] agesV2 = new int[3];  //getrennt: 1. Anzahl festlegen
-            agesV2[0] = 9;               //          2. befüllen
+            int[] agesV2 = new int[3];  //getrennt: 1. Anzahl festlegen DECLARE
+            agesV2[0] = 9;              //          2. befüllen         SET / ASSIGN VALUES
             agesV2[1] = 49;
             agesV2[2] = 44;
 
-            Console.WriteLine("Schalömsche, " + names[1] + "! Ich hoffe, Du freust Dich auf Deinen " + (ages[1] + 1) + ". Geburtstag :)");
+            int[] agesV3 = { 9, 49, 44 };   // (auch alte Version)
+            int[] agesV4 = [ 9, 49, 44 ];   // INITIALIZE (neue Version lt MSLearn)
+
+            Console.WriteLine($"Schalömsche, {names[1]}! Ich hoffe, Du freust Dich auf Deinen {agesV4[1] + 1}. Geburtstag :)");
+            Console.WriteLine($"In Deiner Familie sind {names.Length} Personen.");
 
 
 
